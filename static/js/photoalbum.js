54b64c4eeba76b908d147758ba88fa4550c5e28c
WilNichols <desk@wilnichols.com>
2017-10-01 14:12:01 -0500

    sep album js file w/ separate pageback/next functions

---
+++ static/js/photoalbum.js
@@ -0,0 +1,108 @@
+$(document).ready(function() {
+    function launchIntoFullscreen(element) {
+        $('body').addClass('fullscreen');
+        if(element.requestFullscreen) {
+            element.requestFullscreen();
+        } else if(element.mozRequestFullScreen) {
+            element.mozRequestFullScreen();
+        } else if(element.webkitRequestFullscreen) {
+            element.webkitRequestFullscreen();
+        } else if(element.msRequestFullscreen) {
+            element.msRequestFullscreen();
+        }
+    }
+    function exitFullscreen() {
+        if(document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if(document.mozCancelFullScreen) {
+            document.mozCancelFullScreen();
+        } else if(document.webkitExitFullscreen) {
+            document.webkitExitFullscreen();
+        }
+    }                                                        
+    $(document).bind('webkitfullscreenchange mozfullscreenchange fullscreenchange', function(e) {
+        var state = document.fullScreen || document.mozFullScreen || document.webkitIsFullScreen;
+        var event = state ? 'FullscreenOn' : 'FullscreenOff';
+        if (event = 'FullscreenOff' && event != 'FullscreenOn') {
+            $('body.fullscreen').removeClass('fullscreen');
+        }
+    });
+    $('.fullscreen-exit').click(function() {
+         exitFullscreen();
+    });
+    function album() {
+        var hash = window.location.href.split('#')[1] || '';
+        var start = $('.photoalbum li:nth-of-type(1)');
+        var photos = $('.photoalbum li').length;
+        var currentphoto = '1';
+        if (hash <= photos && hash > 0 && hash != '')  {
+            start = $('.photoalbum li:nth-of-type(' + hash + ')');
+            currentphoto = hash;
+        }
+        console.log(currentphoto);
+        function updatetext() {
+            $('.counter p').text(currentphoto + '/' + photos);
+        }
+       function pagenext(container) {
+            container.addClass('front').removeClass('next');
+            container.prev().removeClass('front').addClass('prev');
+            container.next().addClass('next').removeClass('dblnext');
+            container.nextAll().eq(1).addClass('dblnext');
+            container.prevAll().eq(1).removeClass('prev').addClass('dblprev');
+            container.prevAll().eq(2).removeClass('dblprev');
+       }
+       function pageback(container) {
+            container.addClass('front').removeClass('prev');
+            container.next().removeClass('front').addClass('next');
+            container.prev().addClass('prev').removeClass('dblprev');
+            container.prevAll().eq(1).addClass('dblprev');
+            container.nextAll().eq(1).removeClass('next').addClass('dblnext');
+            container.nextAll().eq(2).removeClass('dblnext');
+       }
+        updatetext();
+        start.addClass('front');
+        start.next().addClass('next');
+        start.nextAll().eq(1).addClass('dblnext');
+        start.prev().addClass('prev');
+        start.prevAll().eq(1).addClass('dblprev');
+        
+        $(".photoalbum li a").click(function() {
+                                    
+            var hash = $(this).attr('href');
+            history.pushState(null, null, hash);
+            currentphoto = hash.substr(1);
+            var container = $(this).parent('li');
+            
+            if (container.hasClass('next')) {
+                pagenext(container);
+            } else if (container.hasClass('prev')) {
+                pageback((container));
+            } else if (container.hasClass('front')) {
+                launchIntoFullscreen(document.documentElement)
+            }
+            updatetext();
+            return false;
+        });                      
+
+    }
+    function photopreload() {
+        var loader ="<i class='loader' />";
+        // prog. enhancement; only hide images before load IF js is enabled
+        $('.photocontainer a').append(loader);
+        $('.photocontainer a').addClass('preloader');
+        $('#imageloader').imagesLoaded().progress( function( instance, image ) {
+            var url = image.img.src;
+            filename = url.split("/").pop();
+            var img = $("img[src$='" + filename + "']");
+            $(img).parent().removeClass('preloader');
+            //remove loader 
+            $(img).next().remove();
+            $(img).addClass("fade-in");
+        }).done( function( instance ) {
+            console.log( 'all are done!');
+        });
+    }
+    
+    photopreload();
+    album();
+});
